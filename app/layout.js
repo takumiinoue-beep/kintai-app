e47eefe70c25c781a392877db812e231ff55c13e
takumiinoue-beep <takumi.inoue@arbor-t.com>
2026-06-27 19:52:37 +0900

    ファビコンをmetadataで明示指定

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 export const metadata = {
   title: "勤怠管理",
   description: "従業員勤怠管理システム",
+  icons: { icon: '/icon.png' },
 };
 
 export default function RootLayout({ children }) {
